8a05ad854391400894745cdda6c70900ab059993
rapidroutines <rapidroutines@gmail.com>
2025-04-08 18:52:38 -0400

    Create index.jsx

---
+++ src/constants/index.jsx
@@ -0,0 +1,59 @@
+import { 
+  BookOpen, 
+  ChartColumn, 
+  Dumbbell, 
+  Home, 
+  NotepadText, 
+  TreePine, 
+  MessageSquare 
+} 
+  from "lucide-react";
+
+export const navbarLinks = [
+  {
+    title: "Dashboard",
+    links: [
+      {
+        label: "Dashboard",
+        icon: Home,
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "Hot Features",
+    links: [
+      {
+        label: "Chatbot",
+        icon: ChartColumn,
+        path: "/chatbot",
+      },
+      {
+        label: "RepBot",
+        icon: NotepadText,
+        path: "/repbot",
+      },
+    ],
+  },
+  {
+    title: "Fitness Features",
+    links: [
+      {
+        label: "Library",
+        icon: BookOpen,
+        path: "/library",
+      },
+      {
+        label: "Exercise Tracker",
+        icon: Dumbbell,
+        path: "/exercise-tracker",
+      },
+      {
+        label: "Rapidtree",
+        icon: TreePine,
+        path: "/rapidtree",
+      },
+     
+    ],
+  },
+];
