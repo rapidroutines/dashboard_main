7785f2ccde4b4abe0335176a7b5ae5ca650e62d3
rapidroutines <rapidroutines@gmail.com>
2025-04-08 17:33:02 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/contexts/theme-context";
+import Layout from "@/routes/layout";
+import DashboardPage from "@/routes/dashboard/page";
+import ChatbotPage from "@/routes/chatbot/page";
+import RepBotPage from "@/routes/repbot/page";
+import ExerciseTrackerPage from "@/routes/exercise-tracker/page";
+import LibraryPage from "@/routes/library/page";
+import RapidTreePage from "@/routes/rapidtree/page";
+
+function App() {
+    return (
+        <ThemeProvider storageKey="theme">
+            <Router>
+                <Layout>
+                    <Routes>
+                        <Route path="/" element={<DashboardPage />} />
+                        <Route path="/analytics" element={<h1 className="title"></h1>} />
+                        <Route path="/reports" element={<h1 className="title"></h1>} />
+                        <Route path="/library" element={<LibraryPage />} />
+                        <Route path="/exercise-tracker" element={<ExerciseTrackerPage />} />
+                        <Route path="/rapidtree" element={<RapidTreePage />} />
+                        <Route path="/chatbot" element={<ChatbotPage />} />
+                        <Route path="/repbot" element={<RepBotPage />} />
+                        <Route path="*" element={<DashboardPage />} />
+                    </Routes>
+                </Layout>
+            </Router>
+        </ThemeProvider>
+    );
+}
+
+export default App;
