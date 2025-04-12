131da4f3e237e191fe52529e82cd8728c57aa7e1
RapidRoutines AI <rapidroutines@gmail.com>
2025-04-11 23:16:02 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './styles/responsive.css'  // Import our responsive styles
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
