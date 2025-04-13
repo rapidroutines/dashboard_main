7b2806dd23fa2202a619212ff6ddede9b2e4787d
RapidRoutines AI <rapidroutines@gmail.com>
2025-04-13 15:17:18 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ThemeProvider } from '@/contexts/theme-context'
+import { ExerciseProvider } from '@/contexts/exercise-context'
+import { ChatbotProvider } from '@/contexts/chatbot-context'
+import { SavedExercisesProvider } from '@/contexts/saved-exercises-context'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <ThemeProvider>
+      <ExerciseProvider>
+        <ChatbotProvider>
+          <SavedExercisesProvider>
+            <App />
+          </SavedExercisesProvider>
+        </ChatbotProvider>
+      </ExerciseProvider>
+    </ThemeProvider>
+  </StrictMode>
 )
